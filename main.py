14b81a57f1bc358d54d715016cbd917047fbb1a3
ko5 <kotyana1331@gmail.com>
2020-05-09 01:17:06 +0900

    Add !?!?!??!?!??!

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 import discord
 import asyncio
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
 INTRODUCTION = \
 "\
-***Kobot Self Introduction***\n\
-**・Commands**\n\
 ```\
 *timer :ユーザーごとに割り当てられるストップウオッチ\n\
     |-- start\n\
@@ -18,14 +18,14 @@ INTRODUCTION = \
 *status <メンション> :メンションしたユーザーの状態を表示\n\
 \n\
 ```\n\
-まだこれしかできないけど増えるかもしれません\n\
 "
 
 class Kobot(discord.Client):
-    def __init__(self, token, base_channel_id):
+    def __init__(self, token, base_channel_id, base_vc_id):
         super(Kobot, self).__init__()
         self.token = token
         self.base_channel_id = base_channel_id
+        self.base_vc_id = base_vc_id
         self.timer = {}
 
         self.base_channel = None
@@ -55,6 +55,14 @@ class Kobot(discord.Client):
             await message.channel.send('is power')
             return
 
+        if "とは" in message.content:
+            message.content
+
+        if "!?" in message.content and len(self.base_vc_id.members)>=2:
+            await self.base_vc_id.content
+            vc_client = message.guild.voice_client
+            await self.play(vc_client)
+
         if message.content[0]=="*":
             await self.valid_command(message, message.author)
             return
@@ -72,14 +80,14 @@ class Kobot(discord.Client):
 
         if command == "timer":
             if len(user_order) < 2:
-                await message.channel.send(user.mention + "\n入力エラーだよ")
+                await message.channel.send(user.mention + "\n入力エラー")
                 return
             await self.personal_timer(user_order[1], message)
             return
 
         if command == "status":
             if len(user_order) < 2 or user_order[1][1] != "@":
-                await message.channel.send(user.mention + "\n入力エラーだよ")
+                await message.channel.send(user.mention + "\n入力エラー")
                 return
             mentioned_user_id = user_order[1][3:-1]
             for guild_user in message.guild.members:
@@ -92,7 +100,7 @@ class Kobot(discord.Client):
 
     async def personal_timer(self, order_type: str, message: discord.Message):
         """
-        ユーザーごとにストップウォッチ式で時間計測できる
+        ストップウォッチ式で時間計測
         """
         user = message.author
         channel = message.channel
@@ -103,49 +111,49 @@ class Kobot(discord.Client):
         if order_type == "start":
             if user_time == "stop":
                 self.timer[user] = 0
-                await channel.send(user.mention + "\nタイマーをスタートしたよ")
+                await channel.send(user.mention + "\nタイマーをスタートしました")
             elif type(user_time) == int:
-                await channel.send(user.mention + "\nタイマーは既にスタートしているよ")
+                await channel.send(user.mention + "\nタイマーは既にスタートしています")
             else:
                 user_time = int(user_time[5:])
                 self.timer[user] = user_time
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーを経過時間 {} から再スタートしたよ".format(time))
+                await channel.send(user.mention + "\nタイマーを経過時間 {} から再スタート".format(time))
 
         elif order_type == "stop":
             if user_time == "stop":
-                await channel.send(user.mention + "\nタイマーは動いてないよ")
+                await channel.send(user.mention + "\nタイマーは動いていません")
             elif type(user_time) == int:
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーを停止したよ。\n経過時間は {} だよ".format(time))
+                await channel.send(user.mention + "\nタイマーを停止しました。\n経過時間は {}".format(time))
                 self.timer[user] = "stop"
             else:
                 user_time = int(user_time[5:])
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーを停止したよ。\n経過時間は {} だよ".format(time))
+                await channel.send(user.mention + "\nタイマーを停止しました。\n経過時間は {}".format(time))
                 self.timer[user] = "stop"
 
         elif order_type == "preview":
             if user_time == "stop":
-                await channel.send(user.mention + "\nタイマーは動いてないから経過時間は0秒だよ")
+                await channel.send(user.mention + "\nタイマーは動いてないので経過時間は0秒")
             elif type(user_time) == int:
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーの現時点での経過時間は {} だよ".format(time))
+                await channel.send(user.mention + "\nタイマーの現時点での経過時間は {}".format(time))
             else:
                 user_time = int(user_time[5:])
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーは経過時間 {} で停止中だよ".format(time))
+                await channel.send(user.mention + "\nタイマーは経過時間 {} で停止中".format(time))
 
         elif order_type == "pause":
             if type(user_time) == int:
                 time = self.format_time(user_time)
-                await channel.send(user.mention + "\nタイマーを経過時間 {} で一時停止したよ".format(time))
+                await channel.send(user.mention + "\nタイマーを経過時間 {} で一時停止しました".format(time))
                 self.timer[user] = "pause" + str(user_time)
             else:
-                await channel.send(user.mention + "\nタイマーは動いてないよ")
+                await channel.send(user.mention + "\nタイマーは動いていません")
 
         else:
-            await channel.send(user.mention + "\n入力エラーだよ")
+            await channel.send(user.mention + "\n入力エラー")
         return
 
     def format_time(self, second):
@@ -165,7 +173,15 @@ class Kobot(discord.Client):
             result += " : "
         return result[:12]
 
+    def play(self, vc_client):
+        if not vc_client:
+            return
+        audio_source = discord.FFmpegPCMAudio("！？！？！？！？！？！？！！？！？！.mp3")
+        vc_client.play(audio_source)
+
 if __name__ == "__main__":
     TOKEN = os.environ["KOBOT_TOKEN"]
-    KOBOT = Kobot(TOKEN, base_channel_id=701058219111612427)
+    if not discord.opus.is_loaded():
+        discord.opus.load_opus("heroku-buildpack-libopus")
+    KOBOT = Kobot(TOKEN, base_channel_id=701058219111612427, base_vc_id=683939861539192865)
     KOBOT.launch()
